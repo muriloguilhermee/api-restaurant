2ebe4626c81778d76bb6882def99cf23ae1b9760
murilloguilherme <murillo.guilherme3101@gmail.com>
2025-12-23 14:40:51 -0300

    initial project

---
+++ knexfile.ts
@@ -0,0 +1,26 @@
+import { dir } from "console";
+import e from "express";
+import { connect } from "http2";
+
+export default {
+    client: "sqlite3",
+    connection: {
+        filename: "./src/database/database.db"
+    },
+    pool: {
+        afterCreate: (connection: any, done: any) => {
+            connection.run("PRAGMA foreign_keys = ON")
+            done()
+        },
+    },
+
+    useNullAsDefault: true,
+    migrations: {
+        extensions: "ts",
+        directory: "./src/database/migrations"
+    },
+    seeds: {
+        extensions: "ts",
+        directory: "./src/database/seeds",
+    },
+}
